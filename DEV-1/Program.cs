b30621bfa52f7c768f6d900f76515dc76189a2a5
Evgeny Kisel <kisel.jenya@gmail.com>
2017-06-20 15:11:19 +0300

    task with changing numbers

---
+++ DEV-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeNumbersOutput
+{
+    class Program
+    {
+       // Entrypoint to program
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+               String output = (i % 3 == 0 ? "3*" + i / 3 : i.ToString());
+               Console.Write(output + " ");
+            }
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
