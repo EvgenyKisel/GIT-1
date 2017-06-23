1c17f6c6fcd97d8b750d07c5ce604f1a909c8372
Evgeny Kisel <kisel.jenya@gmail.com>
2017-06-23 11:47:21 +0300

    add a program that detect the number of Fibonacci

--- DEV-3/FibonacciNumbers/FibonacciNumbers/Program.cs
+++ DEV-3/FibonacciNumbers/FibonacciNumbers/Program.cs
@@ -19,6 +19,26 @@ namespace FibonacciNumbers
                     }
                     else
                     {
+                        int fibNumb1 = 1, fibNumb2 = 1, fibNumbN = 1;
+                        bool checkFibonacci = false;
+                        for (int i = 0; i < number; i++)
+                        {
+                            fibNumb1 = fibNumb2;
+                            fibNumb2 = fibNumbN;
+                            fibNumbN = fibNumb1 + fibNumb2;
+                            if (number == fibNumbN || number == 1)
+                            {
+                                checkFibonacci = true;
+                            }
+                        }
+                        if (checkFibonacci)
+                        {
+                            Console.WriteLine("This is the number of Fibonacci.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is not the number of Fibonacci.");
+                        }
                         Console.WriteLine("\nPress any key to exit.");
                         Console.ReadKey();
                     }
