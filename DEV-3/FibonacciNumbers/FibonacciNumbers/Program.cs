401ea6c481f9752786274b3d57e374e6539633e7
Evgeny Kisel <kisel.jenya@gmail.com>
2017-06-23 11:22:02 +0300

    add an input of number

--- DEV-3/FibonacciNumbers/FibonacciNumbers/Program.cs
+++ DEV-3/FibonacciNumbers/FibonacciNumbers/Program.cs
@@ -6,6 +6,30 @@ namespace FibonacciNumbers
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter a number: ");
+                    int number = int.Parse(Console.ReadLine());
+                    if (number < 0)
+                    {
+                        Console.WriteLine("It is a negative number. Try again");
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nPress any key to exit.");
+                        Console.ReadKey();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("It is not a number. Try again");
+                    continue;
+                }
+                break;
+            }
         }
     }
 }
