78ce83b2fb878c789fa9f976f33a860338573530
Evgeny Kisel <kisel.jenya@gmail.com>
2017-07-05 01:00:07 +0300

    added restart and check for existance

--- DEV-7/TriangleType/Runner.cs
+++ DEV-7/TriangleType/Runner.cs
@@ -4,22 +4,30 @@ namespace TriangleType
 {
     class Runner
     {
+        const string FORMATEXCEPTION = "It's not a number. Try again";
+        const string RESTART = "\nDo you want to try again? (Esc - try again / other key - restart)";
+
         static void Main(string[] args)
         {
-            try
+            do
             {
-                Triangle triangle = new Triangle();
-                Inputer inputer = new Inputer();
-                triangle.SideA = inputer.InputSide();
-                triangle.SideB = inputer.InputSide();
-                triangle.SideC = inputer.InputSide();
-                
-                Console.ReadKey();
+                try
+                {
+                    Triangle triangle = new Triangle();
+                    Inputer inputer = new Inputer();
+                    triangle.SideA = inputer.InputSide();
+                    triangle.SideB = inputer.InputSide();
+                    triangle.SideC = inputer.InputSide();
+                    triangle.TriangleExistence();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine(FORMATEXCEPTION);
+                    continue;
+                }
+                Console.WriteLine(RESTART);
             }
-            catch 
-            {
-
-            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
 }
--- DEV-7/TriangleType/Triangle.cs
+++ DEV-7/TriangleType/Triangle.cs
@@ -4,6 +4,8 @@ namespace TriangleType
 {
     class Triangle
     {
+        const string DONOTEXIST = "Triangle doesn't exist.";
+
         public double SideA
         { get; set; }
 
@@ -13,6 +15,15 @@ namespace TriangleType
         public double SideC
         { get; set; }
 
-
+        public void TriangleExistence()
+        {
+            double maxSide = Math.Max(Math.Max(SideA, SideB), SideC);
+            double minSide = Math.Min(Math.Min(SideA, SideB), SideC);
+            double meanSide = SideA + SideB + SideC - maxSide - minSide;
+            if (minSide + meanSide <= maxSide)
+            {
+                Console.WriteLine(DONOTEXIST);
+            }
+        }
     }
 }
