5f581e8cf5cbec134bd384dc77806b283e6810e7
Evgeny Kisel <kisel.jenya@gmail.com>
2017-08-08 10:25:03 +0300

    started the program

---
+++ DEV-8/Shop/Entrypoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    class EntryPoint
+    {
+        const string RESTART = "\nDo you want to try again? (Esc - exit / other key - restart)";
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                try
+                {
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                Console.WriteLine(RESTART);
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
