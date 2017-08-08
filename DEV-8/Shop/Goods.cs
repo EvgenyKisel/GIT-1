1a20eea1bb9db5aeed8a55ea47c50f16165eba38
Evgeny Kisel <kisel.jenya@gmail.com>
2017-08-08 10:50:11 +0300

    class Goods was added

---
+++ DEV-8/Shop/Goods.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Shop
+{
+    class Goods
+    {
+        const string INPUTTYPEEXCEPTION = "\n ! Uncorrect type. Use only letters. Try again";
+        const string INPUTCOUNTEXCEPTION = "\n ! Uncorrect count. Count can't be negative. Try again";
+        
+        private string type;
+        private string name;
+        private int count;
+        private double price;
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                if (Regex.IsMatch(value, "^[a-zA-Z ]+$"))
+                {
+                    type = value;
+                }
+                else
+                {
+                    throw new InputException(INPUTTYPEEXCEPTION);
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value; 
+        }
+
+        public int Count
+        {
+            get => count;
+            set
+            {
+                if (count < 0)
+                {
+                    throw new InputException(INPUTCOUNTEXCEPTION);
+                }
+                count = value;
+            }
+        }
+
+        public double Price
+        {
+            get => price;
+            set => price = value;
+        }
+    }
+}
