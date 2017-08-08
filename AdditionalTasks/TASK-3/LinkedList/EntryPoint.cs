330db8efda01808ec8f3a400e8795c4182763957
Evgeny Kisel <kisel.jenya@gmail.com>
2017-08-08 13:16:10 +0300

    list v1.0

---
+++ AdditionalTasks/TASK-3/LinkedList/EntryPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class EntryPoint
+    {
+        const string RESTART = "\nDo you want to try again? (Esc - exit / other key - restart)";
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                try
+                {
+                    LinkedList<int> list = new LinkedList<int>();
+                    list.Add(1);
+                    list.Add(25);
+                    list.Add(-5);
+                    list.Remove(1);
+                    list.Output(list);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                Console.WriteLine(RESTART);
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
