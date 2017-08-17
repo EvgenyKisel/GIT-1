28f6eb35cfa9fef7b7561650db3ac85b3788b1e6
Evgeny Kisel <kisel.jenya@gmail.com>
2017-08-17 14:29:10 +0300

    added menu with three commands and some fixies

--- DEV-8/Shop/Entrypoint.cs
+++ DEV-8/Shop/Entrypoint.cs
@@ -7,13 +7,14 @@ namespace Shop
     {
         const string RESTART = "\nDo you want to try again? (Esc - exit / other key - restart)";
         const string CONTINUEINPUTORNOT = "\nDo you want to add one more product? (Esc - no / other key - yes)";
-       
+
         static void Main(string[] args)
         {
             do
             {
                 try
                 {
+                    Console.Clear();
                     ArrayList goods = new ArrayList();
                     int goodsNumber = 1;
                     do
@@ -34,11 +35,15 @@ namespace Shop
                     while (Console.ReadKey(true).Key != ConsoleKey.Escape);
                     Outputer outputer = new Outputer();
                     outputer.PrintListOfGoods(goods);
+                    Menu menu = new Menu();
+                    menu.DoTheCommand(goods);
                 }
-                catch (Exception)
+                catch (Exception exception)
                 {
+                    Console.WriteLine(exception.Message);
                     continue;
                 }
+                Console.Clear();
                 Console.WriteLine(RESTART);
             }
             while (Console.ReadKey(true).Key != ConsoleKey.Escape);
---
+++ DEV-8/Shop/Menu.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+
+namespace Shop
+{
+    class Menu
+    {
+        public void DisplayMenu()
+        {
+            Console.WriteLine("Available commands: ");
+            Console.WriteLine("  count types - display the number of product types");
+            Console.WriteLine("  count all - display count of products;");
+            Console.WriteLine("  average price - display average price of all products;");
+            Console.WriteLine("  average price type - display average product type price;");
+            Console.WriteLine("  exit - quit the program\n");
+        }
+
+        public void DoTheCommand(ArrayList goods)
+        {
+            const string COUNTTYPES = "count types";
+            const string COUNTALL = "count all";
+            const string AVERAGEPRICE = "average price";
+            const string EXIT = "exit";
+            Console.WriteLine("Press any key to enter menu");
+            Console.ReadKey();
+            Console.Clear();
+            Menu menu = new Menu();
+            menu.DisplayMenu();
+            Inputer inputer = new Inputer();
+            bool continueInput = true;
+            while (continueInput)
+            {
+                string command = inputer.InputString();
+                switch (command)
+                {
+                    case COUNTTYPES:
+                        Console.WriteLine(menu.CountTypes(goods));
+                        break;
+                    case COUNTALL:
+                        Console.WriteLine(menu.CountAll(goods));
+                        break;
+                    case AVERAGEPRICE:
+                        Console.WriteLine(menu.GetAveragePrice(goods));
+                        break;
+                    case EXIT:
+                        continueInput = false;
+                        break;
+                    default:
+                        Console.WriteLine("");
+                        break;
+                }
+            }
+        }
+
+        public int CountTypes(ArrayList list)
+        {
+            return list.Count;
+        }
+
+        public int CountAll(ArrayList list)
+        {
+            int totalCount = 0;
+            foreach (Goods goods in list)
+            {
+                totalCount += goods.Count;
+            }
+            return totalCount;
+        }
+
+        public double GetAveragePrice(ArrayList list)
+        {
+            double averagePrice = 0;
+            double totalPrice = 0;
+            foreach (Goods goods in list)
+            {
+                totalPrice += goods.Price;
+            }
+            averagePrice = totalPrice / list.Count;
+            return averagePrice;
+        }
+
+        public double GetAveragePriceForType(ArrayList list)
+        {
+            return 0;
+        }
+    }
+}
