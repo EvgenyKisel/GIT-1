e230cac648dba83b000f2f45e19f6f71830a5272
Evgeny Kisel <kisel.jenya@gmail.com>
2017-09-27 14:45:57 +0300

    made the project

---
+++ DEV-9/SymbolReplacementInStrings/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SymbolReplacementInStrings
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            const string FIRSTSTRING = "qwertyuiop";
+            const string SECONDSTRING = "asdfghjkl";
+            string firstString = FIRSTSTRING;
+            string secondString = SECONDSTRING;
+            string stringAfterReplacement = new RandomExchangeInStrings().SwapSymbolsInStrings(firstString, secondString);
+            Console.WriteLine(firstString);
+            Console.WriteLine(secondString);
+            Console.WriteLine(stringAfterReplacement);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DEV-9/SymbolReplacementInStrings/RandomExchangeInStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SymbolReplacementInStrings
+{
+    class RandomExchangeInStrings
+    {
+        public string SwapSymbolsInStrings(string firstString, string secondString)
+        {
+            int startIndexInFirstString = new Random().Next(firstString.Length - 1);
+            int startIndexInSecondString = new Random().Next(secondString.Length - 1);
+            int replacementLengthOfFirstString = new Random().Next(firstString.Length - startIndexInFirstString);
+            int replacementLengthOfSecondString = new Random().Next(secondString.Length - startIndexInSecondString);
+            string replacementSymbolsInFirstString = firstString.Substring(startIndexInFirstString, replacementLengthOfFirstString);
+            string replacementSymbolsInSecondString = secondString.Substring(startIndexInSecondString, replacementLengthOfSecondString);
+            return Regex.Replace(firstString, replacementSymbolsInFirstString, replacementSymbolsInSecondString);
+        }
+    }
+}
