205902655bf5f1f06f0c1b95d6ee2ac0c90750b3
Evgeny Kisel <kisel.jenya@gmail.com>
2017-09-30 19:53:59 +0300

    first try

--- DEV-11/StringTransformation/EntryPoint.cs
+++ DEV-11/StringTransformation/EntryPoint.cs
@@ -1,15 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace StringTransformation
 {
-    class Program
+    class EntryPoint
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Entrypoint to program.
+        /// </summary>
+        static void Main()
         {
+            Dictionary<char, string> latinSymbols = new Dictionary<char, string>
+            {
+                ['а'] = "a",
+                ['б'] = "b",
+                ['в'] = "v",
+                ['г'] = "g",
+                ['д'] = "d",
+                ['е'] = "e",
+                ['ё'] = "e",
+                ['ж'] = "zh",
+                ['з'] = "z",
+                ['и'] = "i",
+                ['й'] = "y",
+                ['к'] = "k",
+                ['л'] = "l",
+                ['м'] = "m",
+                ['н'] = "n",
+                ['о'] = "o",
+                ['п'] = "p",
+                ['р'] = "r",
+                ['с'] = "s",
+                ['т'] = "t",
+                ['у'] = "u",
+                ['ф'] = "f",
+                ['х'] = "kh",
+                ['ц'] = "ts",
+                ['ч'] = "ch",
+                ['ш'] = "sh",
+                ['ы'] = "y",
+                ['э'] = "e",
+                ['ю'] = "yu",
+                ['я'] = "ya"
+            };
+
+            Dictionary<string, char> cyrillicSymbols = new Dictionary<string, char>
+            {
+                ["a"] = 'а',
+                ["b"] = 'б',
+                ["v"] = 'в',
+                ["g"] = 'г',
+                ["d"] = 'д',
+                ["e"] = 'е',
+                ["e"] = 'ё',
+                ["zh"] = 'ж',
+                ["z"] = 'з',
+                ["i"] = 'и',
+                ["y"] = 'й',
+                ["k"] = 'к',
+                ["l"] = 'л',
+                ["m"] = 'м',
+                ["n"] = 'н',
+                ["o"] = 'о',
+                ["p"] = 'п',
+                ["r"] = 'р',
+                ["s"] = 'с',
+                ["t"] = 'т',
+                ["u"] = 'у',
+                ["f"] = 'ф',
+                ["kh"] = 'х',
+                ["ts"] = 'ц',
+                ["ch"] = 'ч',
+                ["sh"] = 'ш',
+                ["y"] = 'ы',
+                ["e"] = 'э',
+                ["yu"] = 'ю',
+                ["ya"] = 'я'
+            };
+
+            
+            string word = "word";
+            
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                foreach (string key in cyrillicSymbols.Keys) {
+                    if (word[i] == key)
+            }
+
+
         }
     }
 }
