6f7acabed4715a2021dcaf02550f744426783590
Evgeny Kisel <kisel.jenya@gmail.com>
2017-10-29 14:29:12 +0300

    made oop

--- DEV-10/FloatArrays/EntryPoint.cs
+++ DEV-10/FloatArrays/EntryPoint.cs
@@ -1,42 +1,31 @@
 ﻿using System;
-using System.Collections;
 
 namespace FloatArrays
 {
+  /// <summary>
+  /// Entrypoint to program.
+  /// </summary>
+  class EntryPoint
+  {
     /// <summary>
-    /// Entrypoint to program.
+    /// This method calls one method, that initializes array of arrays, another method,
+    /// that prints this array on console, and method, that create new array of special values, and then displayed on console.
     /// </summary>
-    class EntryPoint
+    static void Main()
     {
-        const string NEWARRAYISEMPTY = "There is no such elements.";
-
-        /// <summary>
-        /// This method calls one method, that initializes array of arrays, another method,
-        /// that prints this array on console, and method, that create new array of special values, and then displayed on console.
-        /// </summary>
-        static void Main()
-        {
-            try
-            {
-                Initializer initializer = new Initializer();
-                initializer.InitializeFloatArrays(out double[][] floatArrays);
-                Outputer outputer = new Outputer();
-                outputer.OutputFloatArrays(floatArrays);
-                ArrayList newFloatArray = new NewArrayCreator().CreateNewArray(floatArrays);
-                if (new Checker().CheckForNonEmpty(newFloatArray) == true)
-                {
-                    outputer.OutputNewFloatArray(newFloatArray);
-                }
-                else
-                {
-                    Console.WriteLine(NEWARRAYISEMPTY);
-                }
-                Console.ReadKey();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
+      try
+      {
+        FloatArray floatArray = new FloatArray();
+        floatArray.InitializeFloatArrays();
+        floatArray.OutputFloatArrays();
+        floatArray.CreateNewArray();
+        floatArray.OutputNewFloatArray();
+        Console.ReadKey();
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
     }
+  }
 }
---
+++ DEV-10/FloatArrays/FloatArray.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace FloatArrays
+{
+  class FloatArray
+  {
+    const float EPSILON = float.Epsilon;
+    const int RANDOMMINSIZEVALUE = 1;
+    const int RANDOMMAXSIZEVALUE = 10;
+    const int RANDOMMINELEMENTVALUE = -20;
+    const int RANDOMMAXELEMENTVALUE = 50;
+
+    public double[][] FloatArrays { get; set; }
+    public ArrayList NewFloatArray { get; set; }
+
+    /// <summary>
+    /// This method declares and initializes array of arrays with random length and elements.
+    /// </summary>
+    /// <param name="floatArrays"> array of arrays </param>
+    public void InitializeFloatArrays()
+    {
+      Random random = new Random();
+      FloatArrays = new double[random.Next(RANDOMMINSIZEVALUE, RANDOMMAXSIZEVALUE)][];
+      for (int i = 0; i < FloatArrays.Length; i++)
+      {
+        FloatArrays[i] = new double[random.Next(RANDOMMINSIZEVALUE, RANDOMMAXSIZEVALUE)];
+        for (int j = 0; j < FloatArrays[i].Length; j++)
+        {
+          FloatArrays[i][j] = random.NextDouble() * random.Next(RANDOMMINELEMENTVALUE, RANDOMMAXELEMENTVALUE);
+        }
+      }
+    }
+
+    /// <summary>
+    /// This method finds elements, that belong to at least two arrays,
+    /// and then writes them in new array.
+    /// </summary>
+    /// <param name="floatArrays"> array of arrays </param>
+    /// <returns> new array with special elements </returns>
+    public void CreateNewArray()
+    {
+      NewFloatArray = new ArrayList();
+      for (int i = 0; i < FloatArrays.Length - 1; i++)
+      {
+        for (int j = 0; j < FloatArrays[i].Length; j++)
+        {
+          int count = 1;
+          for (int l = i + 1; l < FloatArrays.Length; l++)
+          {
+            for (int k = 0; k < FloatArrays[l].Length; k++)
+            {
+              if (Math.Abs(FloatArrays[i][j] - FloatArrays[l][k]) < EPSILON)
+              {
+                count++;
+                break;
+              }
+            }
+            if (count >= 2)
+            {
+              if (!NewFloatArray.Contains(FloatArrays[i][j]))
+              {
+                NewFloatArray.Add(FloatArrays[i][j]);
+              }
+              break;
+            }
+          }
+        }
+      }
+    }
+
+    /// <summary>
+    /// This method prints on console array of arrays.
+    /// </summary>
+    /// <param name="floatArrays"> array of arrays </param>
+    public void OutputFloatArrays()
+    {
+      foreach (double[] row in FloatArrays)
+      {
+        foreach (float element in row)
+        {
+          Console.Write($"{element} \t");
+        }
+        Console.WriteLine();
+      }
+    }
+
+    /// <summary>
+    /// This method prints on console new array.
+    /// </summary>
+    /// <param name="newFloatArray"> new array with special elements </param>
+    public void OutputNewFloatArray()
+    {
+      StringBuilder line = new StringBuilder();
+      Console.WriteLine();
+      foreach (double element in NewFloatArray)
+      {
+        line.Append($"{element} \t");
+      }
+      Console.WriteLine(line);
+    }
+  }
+}
