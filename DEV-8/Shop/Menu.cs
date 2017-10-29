c2206ceef19f9dddc7c9b491cfc888c2823b4129
Evgeny Kisel <kisel.jenya@gmail.com>
2017-10-29 15:14:55 +0300

    fixies

--- DEV-8/Shop/Menu.cs
+++ DEV-8/Shop/Menu.cs
@@ -2,23 +2,21 @@
 
 namespace Shop
 {
-    class Menu
+  class Menu
+  {
+    const string MENU = "Press any key to start enter commands";
+
+    public void DisplayMenu()
     {
-        const string MENU = "Press any key to start enter commands";
-
-        public void DisplayMenu()
-        {
-            Console.WriteLine(MENU);
-            Console.ReadKey();
-            Console.Clear();
-            Console.WriteLine("Available commands: ");
-            Console.WriteLine("  count types - display the number of product types");
-            Console.WriteLine("  count all - display count of products;");
-            Console.WriteLine("  average price - display average price of all products;");
-            Console.WriteLine("  average price type - display average product type price;");
-            Console.WriteLine("  exit - quit the program\n");
-        }
-
-        public 
+      Console.WriteLine(MENU);
+      Console.ReadKey();
+      Console.Clear();
+      Console.WriteLine("Available commands: ");
+      Console.WriteLine("  count types - display the number of product types");
+      Console.WriteLine("  count all - display count of products;");
+      Console.WriteLine("  average price - display average price of all products;");
+      Console.WriteLine("  average price type - display average product type price;");
+      Console.WriteLine("  exit - quit the program\n");
     }
+  }
 }
