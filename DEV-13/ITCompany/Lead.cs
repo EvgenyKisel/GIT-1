bba07dd5e8ba3ad63127cddcfbe829f3afa09fef
Evgeny Kisel <kisel.jenya@gmail.com>
2017-11-02 17:52:13 +0300

    added employees

---
+++ DEV-13/ITCompany/Lead.cs
@@ -0,0 +1,40 @@
+﻿namespace ITCompany
+{
+  class Lead
+  {
+    private double salary;
+    private int productivity;
+
+    public double Salary
+    {
+      get
+      {
+        return salary;
+      }
+      set
+      {
+        if (value < 0)
+        {
+          throw new InvalidSalaryException();
+        }
+        salary = value;
+      }
+    }
+
+    public int Productivity
+    {
+      get
+      {
+        return productivity;
+      }
+      set
+      {
+        if (value < 0 || value > 10)
+        {
+          throw new InvalidSalaryException();
+        }
+        productivity = value;
+      }
+    }
+  }
+}
