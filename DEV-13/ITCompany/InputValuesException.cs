a7c2fa535501f98d46018e4eebed46180411822a
Evgeny Kisel <kisel.jenya@gmail.com>
2017-11-08 17:32:09 +0300

    added one exception class

---
+++ DEV-13/ITCompany/InputValuesException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ITCompany
+{
+  class InputValuesException : Exception
+  {
+    const string INPUTVALUESERROR = "Wrong value. Try positive values";
+
+    public InputValuesException(string message = INPUTVALUESERROR) : base(message) { }
+  }
+}
