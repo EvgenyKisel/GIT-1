4437371e799bf35f185332aee2ce4753b4e5dc17
Evgeny Kisel <kisel.jenya@gmail.com>
2017-12-15 13:54:48 +0300

    added propierties to user class

--- WordPress/WordPress/Objects/User.cs
+++ WordPress/WordPress/Objects/User.cs
@@ -1,13 +1,59 @@
-﻿namespace WordPress
+﻿using System.Text.RegularExpressions;
+using System;
+
+namespace WordPress
 {
   /// <summary>
   /// This is class, that contains user information.
   /// </summary>
   class User
   {
-    public string UserName { get; set; }
+    private string userName;
+    private string email;
+
+    /// <summary>
+    /// Username can has only alphanumeric characters, spaces, underscores, hyphens, periods and the @ symbol.
+    /// </summary>
+    public string UserName
+    {
+      get
+      {
+        return userName;
+      }
+      set
+      {
+        if (Regex.IsMatch(value, "[0-9a-zA-Z.-_@ ]+"))
+        {
+          userName = value;
+        }
+        else
+        {
+          throw new FormatException();
+        }
+      }
+    }
+    /// <summary>
+    /// Username must be like "username@gmail.com". Username can has only alphanumeric characters, underscores, and hyphens. 
+    /// </summary>
+    public string Email
+    {
+      get
+      {
+        return email;
+      }
+      set
+      {
+        if (Regex.IsMatch(value, "[0-9a-zA-Z.-_]+@[a-zA-Z.]+"))
+        {
+          email = value;
+        }
+        else
+        {
+          throw new FormatException();
+        }
+      }
+    }
     public string Password { get; set; }
-    public string Email { get; set; }
 
     /// <summary>
     /// This is constructor for User class.
